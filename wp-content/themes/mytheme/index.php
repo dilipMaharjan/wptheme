c1099e02d858a16cbca0c2d4b9209d0243481130
crazdrms <dilipmlove@gmail.com>
2016-12-09 22:48:41 +0545

    Initial main template index.php

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -1,8 +1,23 @@
 <?php
+/* Main Template file */
+get_header();
+?>
+<div class="main-content-width-wrapper">
+    <div class="two-column-entry">
+        <h1><?php echo get_the_title(); ?></h1>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        <main class="main-content">
+            <?php
+            //start loop
+            if (have_posts()):
+                while (have_posts()):
+                    the_post();
+                    the_content();
+                endwhile;
+            endif;
+            ?>
+        </main>
+    </div>
+</div>
+<?php get_footer(); ?>
 
