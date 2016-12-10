a0f1e2c1720ae712e80a7c4fba1c158c9af4fc4f
crazdrms <dilipmlove@gmail.com>
2016-12-10 18:40:13 +0545

    added header and footer menu

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -1,11 +1,9 @@
-<?php //header template      ?>
+  <?php //header template      ?>
 <!DOCTYPE html>
 <html lang="en">
 
     <head>
         <meta charset="UTF-8">
-        <link href="<?php bloginfo('template_url'); ?>/style.css" type="text/css" rel=stylesheet>
-        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,400i,700" rel="stylesheet">
         <?php wp_head(); ?>
     </head>
 
@@ -15,10 +13,11 @@
                 <a href="index.html"><img src="<?php bloginfo('template_url'); ?>/images/glass-eye-logo.png" alt="" class="logo"></a>
                     <?php
                     wp_nav_menu($args = array(
-                        'menu_class' => 'main_navigation',
+                        'menu_class' => 'main-navigation',
                         'theme_location' => 'primary'
                     ));
                     ?>
             </nav>
         </header>
 
+ 
