96db56498815fbcd56d1d0e0f0e0e2687e64bca5
crazdrms <dilipmlove@gmail.com>
2016-12-10 21:21:45 +0545

    dynamic sidebar

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -5,7 +5,7 @@
 if (!function_exists('glasseye_setup')):
 
     function glasseye_setup() {
-        //let WordPress handle title tags
+//let WordPress handle title tags
         add_theme_support('title-tag');
     }
 
@@ -27,10 +27,27 @@ add_action('init', 'register_glasseye_menus');
 
 function glasseye_scripts() {
 
-    //enqueue stylesheet
+//enqueue stylesheet
     wp_enqueue_style('glasseye_styles', get_stylesheet_uri());
 //enqueue google fonts
     wp_enqueue_style('glasseye_google_fonts', "https://fonts.googleapis.com/css?family=Raleway:300,400,400i,700");
 }
 
 add_action('wp_enqueue_scripts', 'glasseye_scripts');
+
+/* --- Register widget area --- */
+
+function glasseye_widgets_init() {
+    register_sidebar(array(
+        'name' => __('Main Sidebar', 'wptheme'),
+        'id' => 'main-sidebar',
+        'description' => __('Widgets added here will apear in all pages using the two column template.', 'wptheme'),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget' => '</section>',
+        'before_title' => '<h2 class="widget-title">',
+        'after_title' => '</h2>',
+    ));
+}
+
+add_action('widgets_init', 'glasseye_widgets_init');
+
---
+++ wp-content/themes/mytheme/sidebar.php
@@ -0,0 +1,10 @@
+<?php
+/* The template for the main sidebar */
+?>
+<?php if (is_active_sidebar('main-sidebar')): ?>
+    <aside class="sidebar widget-area">
+        <?php dynamic_sidebar(); ?>
+    </aside>
+<?php endif; ?>
+
+
